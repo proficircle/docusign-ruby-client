6c910cc163ff3bf78e0fc6f6ef68c78463d44d2e
rdinca <radudinca@yahoo.com>
2021-05-10 10:12:59 +0300

    URI::Parser.escape

--- lib/docusign_esign/configuration.rb
+++ lib/docusign_esign/configuration.rb
@@ -168,7 +168,7 @@ module DocuSign_eSign
 
     def base_url
       url = "#{scheme}://#{[host, base_path].join('/').gsub(/\/+/, '/')}".sub(/\/+\z/, '')
-      URI.escape(url)
+      URI::Parser.escape(url)
     end
 
     # Gets API key (with prefix if set).
